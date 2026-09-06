87e476b3ed696a29659ae927b134baa073c00ae1
gio mustard <mssm011009@gmail.com>
2026-09-06 12:01:51 -0700

    fix: type of shared modules

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,7 +1,7 @@
-
-export const API_URL =
+export const API_URL: string =
   process.env.EXPO_PUBLIC_API_URL ||
   process.env.NEXT_PUBLIC_API_URL ||
   'http://127.0.0.1:8000';
 
-  export const MONEDA = "MXN";
+export const MONEDA = "MXN" as const;
+export type Moneda = typeof MONEDA;
